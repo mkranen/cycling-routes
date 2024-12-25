044c9e3b17b03b879fe13a715cf4cf40e27b7846
Martijn Kranen <m.kranen@gmail.com>
2024-12-25 19:24:04 +0100

    Basic screen layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,11 @@ import Map from "./Map";
 
 function App() {
   return (
-    <div>
-      <Map />
+    <div className="h-dvh w-dvw">
+      <h1 className="flex items-center h-16 p-4 text-3xl">Cycling routes</h1>
+      <div className="absolute bottom-0 left-0 right-0 top-16">
+        <Map />
+      </div>
     </div>
   );
 }
--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -8,7 +8,12 @@ const Map = () => {
   const longitude = 5.13381188435385;
 
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{ height: "100vh", width: "100vw" }}>
+    <MapContainer
+      center={[latitude, longitude]}
+      zoom={13}
+      ref={mapRef}
+      className="absolute top-0 bottom-0 left-0 right-0 w-full h-full"
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
